dc4abd1f59e59afbca5a5d79ba663fa7502cf66e
Hardmath123 <hardmath123@gmail.com>
2014-06-04 14:28:27 -0700

    Yay CLI

--- cli.js
+++ cli.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
+
+var opts = require("nomnom")
+    .option('input', {
+        position: 0,
+        help: 'input file'
+    })
+    .option('output', {
+        abbr: 'o',
+        help: 'output file'
+    })
+    .option('ansi', {
+        abbr: 'a',
+        help: 'Trigger ANSI mode',
+        flag: true,
+        default: false
+    })
+    .option('version', {
+        abbr: 'v',
+        help: 'Print version and exit',
+        flag: 'true',
+        callback: function() {
+            return require("./package.json").version;
+        }
+    }).parse();
+
+var fs = require("fs");
 var l = require("./lowtex.js");
 var c = new l();
-process.stdin.pipe(c);
-c.pipe(process.stdout);
+
+(opts.input ? fs.createReadStream(opts.input) : process.stdin).pipe(c);
+c.pipe(opts.output ? fs.createWriteStream(opts.output) : process.stdout);
